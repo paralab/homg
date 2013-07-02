36adc7c3dcfafa31147268fd16899da44ef3534a
Hari Sundar <hari@ices.utexas.edu>
2013-07-02 10:50:57 -0500

    minor to colocated

--- create_colocated_grids.m
+++ create_colocated_grids.m
@@ -5,9 +5,10 @@ function grid = create_colocated_grids (dim, geom, order, nelems, sp)
 %% create linear grid heirarchy
 lin_elems = order*nelems;
 
-lin_grid = create_grid_hierarchy(dim, geom, 1, lin_elems, sp)
+lin_grid = create_grid_hierarchy(dim, geom, 1, lin_elems, sp);
 
 %% create ho grid,
+disp(['Creating ho-grid: order = ' num2str(order) ', nelem = ' num2str(nelems(end))]);
 m = homg.mesh(dim, geom, nelems(end), sp);
 grid = homg.grid(m, order, lin_grid);
 
