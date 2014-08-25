b1b5c4e752d0495f34e6ca49929b313858e50a93
tanbui <tanbui@ices.utexas.edu>
2014-08-25 13:53:48 -0500

    add correct error norm

--- +homg/grid.m
+++ +homg/grid.m
@@ -276,7 +276,8 @@ classdef grid < handle
             
             % BData = zeros(size(grid.Bmaps));
             % 1. compute skeletal trace
-            u_hat = grid.extract_skeletal_data(u);
+            u_hat_t = grid.extract_skeletal_data(u);
+            u_hat = zeros(size(u_hat_t));
             b = -grid.hdg_residual(u_hat, rhs, BData);
 
             % 2. iterate - vcycles 
@@ -289,7 +290,9 @@ classdef grid < handle
             for i=1:num_vcyc
                 u_hat = u_hat + grid.vcycle_hdg(v1, v2, r, u_hat);
                 r = grid.residual(b, u_hat);
-                disp([num2str(i, '%03d\t') ': |res| = ' num2str(norm(r),'\t%8.4e')]);
+                temp = u_hat - u_hat_t;
+                err = sqrt(temp.' * grid.K * temp);
+                disp([num2str(i, '%03d\t') ': |res| = ' num2str(norm(err),'\t%8.4e')]);
                 if (norm(r)/r0 < 1e-8)
                     iter = i;
                     rr = norm(r)/r0;
